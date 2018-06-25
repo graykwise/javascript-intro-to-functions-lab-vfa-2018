d9f078c130db5e066ea6eed500e5a5b9dfb50c49
Grayson Wise <graysonwise3@gmail.com>
2018-06-25 22:32:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  return console.log(shout(string))
+}
+
+function logWhisper(string) {
+  return console.log(whisper(string))
+}
